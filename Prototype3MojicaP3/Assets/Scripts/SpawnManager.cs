79d388160cac0da84b9877a7cb59898469ea079d
JJmojica23 <jjmojica2015@gmail.com>
2023-03-29 11:41:58 -0700

    Added animations and particles
    There are now animations for walking, jumping, and dying. Adjusted gravity multiplier, mass, and jump force for the jumping animation. This also goes for the animation speed. Added a particle and modified it. It now plays when the player collides with a boulder.

--- Prototype3MojicaP3/Assets/Scripts/SpawnManager.cs
+++ Prototype3MojicaP3/Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,28 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(35, 2, 0);
+    private float startDelay = 2;
+    private float repeatRate = 3;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle ()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
